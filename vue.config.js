b555fd1353e3a6db7b97524bdd82ad78ffc0f225
zhaoaijing <1598949225@qq.com>
2022-09-30 16:52:54 +0800

    更新page

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,11 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir:'docs',
   productionSourceMap: false,
+  pages: {
+    index: {
+      entry: 'examples/main.js'
+    }
+  }
 }
 // gzip压缩
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
